80b4e980c1aad88efd0f602e34707dac2272fd90
matan <mbardach@valerahealth.com>
2017-02-15 12:27:05 +0200

    add compression to dependencies

--- dist-server/server.js
+++ dist-server/server.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-//const compression = require('compression');
+const compression = require('compression');
 
 
 const port = process.env.PORT || 3000;
 
-//app.use(compression());
+app.use(compression());
 app.use(express.static(path.join( __dirname, '../dist/')));
 app.use('/*', express.static(path.join( __dirname, '../dist/index.html')));
 
